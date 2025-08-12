6ccacbbc2f783071a52cbf96d63c53c9212b3d50
mzzhan <mzzhan@gizwits.com>
2025-08-12 12:10:33 +0800

    增加一些常用脚本

---
+++ 0env.sh
@@ -0,0 +1,1 @@
+python scripts/release/release.py
---
+++ export_adf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$USER" == "martin" ]; then
+    IDF_VERSION="v5.3.2"
+
+    # export ADF_PATH="$HOME/esp32/esp-adf-newest"
+    export ADF_PATH="$HOME/esp32/esp-adf-gizwits"
+    # export ADF_PATH="$HOME/giz_adf/esp-adf"
+    export IDF_PATH="$ADF_PATH/esp-idf"
+    ESP_INSTALL_DIR="$HOME/.espressif-$IDF_VERSION"
+
+    export PATH=${PATH}:${IDF_PATH}/tools/
+
+    # 创建指向esp-idf软链接
+    ESP_DIR="$HOME/.espressif"
+
+    if [ -L "$ESP_DIR" ]; then
+        echo "Delete link to ~/.espressif"
+        rm "$ESP_DIR"
+    fi
+
+    if [ ! -e "$ESP_DIR" ]; then
+        echo "Recreate $ESP_DIR link to $ESP_INSTALL_DIR"
+        ln -s "$ESP_INSTALL_DIR" "$ESP_DIR"
+    fi
+elif [ "$USER" == "root" ]; then
+    export ADF_PATH="$HOME/esp/esp-adf-54"
+    export IDF_PATH="$ADF_PATH/esp-idf"
+fi
+
+
+# call '. export_idf.sh' in bash
+. "${ADF_PATH}/export.sh"
