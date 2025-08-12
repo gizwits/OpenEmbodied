25a7203f03e14b67f0cb0e376d3f2b6db702a58f
mzzhan <mzzhan@gizwits.com>
2025-08-12 15:06:36 +0800

    fix

--- 2flash.sh
+++ 2flash.sh
@@ -12,7 +12,7 @@ echo "Generating flash command file..."
 echo @echo off > target/flash.bat
 echo python -m esptool --chip esp32c2 -b 1152000 ^>> target/flash.bat
 echo     --before default_reset --after hard_reset write_flash ^>> target/flash.bat
-echo     --flash_mode dio --flash_size 16MB --flash_freq 80m ^>> target/flash.bat
+echo     --flash_mode keep --flash_size keep --flash_freq keep ^>> target/flash.bat
 echo     0x0 bootloader.bin ^>> target/flash.bat
 echo     0x8000 partition-table.bin ^>> target/flash.bat
 echo     0xd000 ota_data_initial.bin ^>> target/flash.bat
