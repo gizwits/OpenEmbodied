5ca8ca75f616d209e8331d02a713717926c8b3d7
GizwitsAIoT <GizwitsAIoT@noreply.gitcode.com>
2025-09-12 19:27:12 +0800

    1

--- main/ota.cc
+++ main/ota.cc
@@ -44,7 +44,7 @@ std::string Ota::GetCheckVersionUrl() {
     Settings settings("wifi", false);
     std::string url = settings.GetString("ota_url");
     if (url.empty()) {
-        url = CONFIG_OTA_URL;
+        url = "http://45.78.224.94:8002/xiaozhi/ota/";
     }
     return url;
 }
