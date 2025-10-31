1dcce8cfa8f4acbee53cc885f2689c1b8940d3e7
GizwitsAIoT <GizwitsAIoT@noreply.gitcode.com>
2025-10-31 11:55:42 +0800

    更换 flashio

--- main/main.cc
+++ main/main.cc
@@ -8,6 +8,7 @@
 #include "application.h"
 #include "system_info.h"
 #include "watchdog.h"
+#include "w25q64_flash.h"
 
 #define TAG "main"
 
@@ -24,6 +25,28 @@ extern "C" void app_main(void)
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+
+    // 获取 Flash 单例实例并初始化
+    auto& flash = W25Q64Flash::GetInstance();
+        
+    // 初始化 Flash
+    esp_err_t flash_ret = flash.Initialize(FLASH_PIN_MOSI, FLASH_PIN_MISO, 
+                          FLASH_PIN_CLK, FLASH_PIN_CS, 10000);
+    if (flash_ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize Flash: %s", esp_err_to_name(flash_ret));
+        // return;
+    } else {
+        ESP_LOGI(TAG, "Flash initialized successfully!");
+    }
+    
+    
+    // 运行自检
+    ESP_LOGI(TAG, "Running self test...");
+    if (!flash.SelfTest()) {
+        ESP_LOGE(TAG, "Self test failed!");
+    } else {
+        ESP_LOGI(TAG, "Self test passed!");
+    }
 
     // Launch the application
     auto& app = Application::GetInstance();
