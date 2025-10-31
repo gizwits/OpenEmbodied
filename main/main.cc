56130b7e83f0666da24996bd9ed50b1f61ce99f7
GizwitsAIoT <GizwitsAIoT@noreply.gitcode.com>
2025-10-31 12:04:03 +0800

    临时禁用外置 flash

--- main/main.cc
+++ main/main.cc
@@ -27,26 +27,26 @@ extern "C" void app_main(void)
     ESP_ERROR_CHECK(ret);
 
     // 获取 Flash 单例实例并初始化
-    auto& flash = W25Q64Flash::GetInstance();
+    // auto& flash = W25Q64Flash::GetInstance();
         
-    // 初始化 Flash
-    esp_err_t flash_ret = flash.Initialize(FLASH_PIN_MOSI, FLASH_PIN_MISO, 
-                          FLASH_PIN_CLK, FLASH_PIN_CS, 10000);
-    if (flash_ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to initialize Flash: %s", esp_err_to_name(flash_ret));
-        // return;
-    } else {
-        ESP_LOGI(TAG, "Flash initialized successfully!");
-    }
+    // // 初始化 Flash
+    // esp_err_t flash_ret = flash.Initialize(FLASH_PIN_MOSI, FLASH_PIN_MISO, 
+    //                       FLASH_PIN_CLK, FLASH_PIN_CS, 10000);
+    // if (flash_ret != ESP_OK) {
+    //     ESP_LOGE(TAG, "Failed to initialize Flash: %s", esp_err_to_name(flash_ret));
+    //     // return;
+    // } else {
+    //     ESP_LOGI(TAG, "Flash initialized successfully!");
+    // }
     
     
-    // 运行自检
-    ESP_LOGI(TAG, "Running self test...");
-    if (!flash.SelfTest()) {
-        ESP_LOGE(TAG, "Self test failed!");
-    } else {
-        ESP_LOGI(TAG, "Self test passed!");
-    }
+    // // 运行自检
+    // ESP_LOGI(TAG, "Running self test...");
+    // if (!flash.SelfTest()) {
+    //     ESP_LOGE(TAG, "Self test failed!");
+    // } else {
+    //     ESP_LOGI(TAG, "Self test passed!");
+    // }
 
     // Launch the application
     auto& app = Application::GetInstance();
